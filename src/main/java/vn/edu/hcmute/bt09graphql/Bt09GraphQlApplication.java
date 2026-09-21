166c71211572aaa56905d7881a07ae5797d64b3f
Quan <qm33165@gmail.com>
2026-09-21 08:09:05 +0700

    Initial Spring Boot GraphQL project

---
+++ src/main/java/vn/edu/hcmute/bt09graphql/Bt09GraphQlApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmute.bt09graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bt09GraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bt09GraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/hcmute/bt09graphql/Bt09GraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmute.bt09graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bt09GraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
